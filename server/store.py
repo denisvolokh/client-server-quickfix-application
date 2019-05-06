cd48e2be766af30a90d4e6f6ea280508ec9e1bc7
Denis Volokh <denis.volokh@gmail.com>
2019-05-07 00:31:06 +0300

    Added store class

---
+++ server/store.py
@@ -0,0 +1,11 @@
+
+class Store(object):
+
+    def __init__(self):
+        self.data = {}
+
+    def put(self, key, value):
+        self.data[key] = value
+
+    def get(self, key):
+        return self.data.get(key, None)
